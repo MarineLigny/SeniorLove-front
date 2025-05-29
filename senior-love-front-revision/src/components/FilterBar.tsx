38817b17f4c24a3af5e5bcac65a82d16b0dcfe2c
Marine Ligny <mligny.dev@gmail.com>
2025-05-29 16:34:57 +0200

    search bar + css meet, still need to format the css of the search because of bug and update the back with Manue

--- senior-love-front-revision/src/components/FilterBar.tsx
+++ senior-love-front-revision/src/components/FilterBar.tsx
@@ -1,17 +1,69 @@
+
+import axios from 'axios';
+import { Search, User } from 'lucide-react';
+import { useState } from 'react';
+import UserCard from './UserCard';
+
+
+
 export default function FilterBar() {
+    const [input, setInput] = useState("");
+    const storedToken = localStorage.getItem("token");
+    const [results, setResults] = useState([]);
+    const [categories, setCategories] = useState("");
+
+    const inputData = async (localisation: string) => {
+        try {
+            const response = await axios.get(
+                "http://marineligny-server.eddi.cloud/meet",
+                {
+                    headers: {
+                        Authorization: `Bearer ${storedToken}`,
+                    },
+                    params: {
+                        localisation, // ← sera transmis en ?localisation=...
+                    },
+                },
+            );
+            console.log(response.data)
+            setResults(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleInputChange = (value: string) => {
+        setInput(value);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        inputData(input);
+        console.log("Recherche envoyée avec :", input, "et catégorie :", categories);
+    };
+
     return (
         <div>
-            <div className="searchEvent">
-            <form action="">
-                    <input className="search" type="search" placeholder="Recherche par mots clés" />
-                    <select className="localisation" name="localisation" id="localisation">
-                        <option value="" disabled selected hidden>localisation</option>
-                        <option value="local">local</option>
-                        <option value="local">local</option>
-                        <option value="local">local</option>
-                        <option value="local">local</option>
-                    </select>
-                    <select className="category" name="category" id="category">
+            <div className="searchbar container">
+                <form action="" className='searchbar-form' onSubmit={handleSubmit}>
+                    <div className='searchbar-input'>
+                        <Search />
+                        <input
+                            className="searchbar-input-text"
+                            type="search"
+                            value={input}
+                            onChange={(e) => handleInputChange(e.target.value)}
+                            name="search"
+                            placeholder="Recherche par mots clés" />
+                    </div>
+
+                    <select
+                        className="category"
+                        name="category"
+                        id="category"
+                        value={categories}
+                        onChange={(e) => setCategories(e.target.value)}
+                    >
                         <option value="" disabled selected hidden>categories</option>
                         <option value="sport">Sport</option>
                         <option value="musique">Musique</option>
@@ -19,8 +71,21 @@ export default function FilterBar() {
                         <option value="loisirs">Loisirs</option>
                     </select>
                 </form>
+            </div>
 
-        </div>
+
+            <div className="allMeet">
+                <section className="title">
+                    <h1>Résultat de la recherche pour '{input}'</h1>
+                </section>
+                <section className="meets">
+                    {results.map((user, index) => (
+                        <div key={index}>
+                            <UserCard user={user} />
+                        </div>
+                    ))}
+                </section>
+            </div>
         </div>
     );
 };
